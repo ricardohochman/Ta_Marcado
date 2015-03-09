42372abcc9a4070125c693eefc3eaa1379c325b7
Vivian Chiodo Dias <vichiodo@MacBook-Pro-de-Vivian.local>
2015-03-09 08:56:04 -0300

    aumentada as labels da tabela

--- Ta_Marcado/Ta_Marcado/TableViewController.m
+++ Ta_Marcado/Ta_Marcado/TableViewController.m
@@ -49,9 +49,6 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     CellViewController *cell = [tableView dequeueReusableCellWithIdentifier:@"listaEnd" forIndexPath:indexPath];
-//    if(!cell){
-//        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"listaEnd"];//rever com os meninos !!!!!
-//    }
     
     long row = [indexPath row];
     MapaPoint *mp = [s.locais objectAtIndex:row];
@@ -70,16 +67,16 @@
 
 
  // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
-     
-     [s removeLocal:[s.locais objectAtIndex:[indexPath row]]];
-     
- [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
- } else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        
+        [s removeLocal:[s.locais objectAtIndex:[indexPath row]]];
+        
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
  }
 
 
